8e0fb9b22ed58063b0e5ba9f3759c8f39d3ba59b
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-10-20 11:35:51 +0200

    first unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,25 @@
+import { AppComponent } from './app.component';
+
+describe(`App Component`, () => {
+  let component: AppComponent;
+
+  beforeEach(() => {
+    component = new AppComponent();
+  });
+
+  it(`should 1 + 1`, () => {
+    expect(1 + 1).toEqual(2);
+  });
+
+  it(`should have a component`, () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have a title of 'app'`, () => {
+    expect(component.title).toEqual('app');
+  });
+});
+
 // import { TestBed, async } from '@angular/core/testing';
 // import { AppComponent } from './app.component';
 // describe('AppComponent', () => {
