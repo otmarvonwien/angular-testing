b3aa65d8bc82f83021939cbac699399501aec09b
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-10-20 14:46:17 +0200

    using real services

--- src/app/containers/users/users.component.spec.ts
+++ src/app/containers/users/users.component.spec.ts
@@ -1,16 +1,20 @@
 import { UsersComponent } from './users.component';
 import { of } from 'rxjs/observable/of';
+import { UserService } from '../../services/user.service';
 
 describe(`Users Component`, () => {
   let component: UsersComponent;
   const fakeUser = {id: 1, name: 'fake'};
-  const fakeUserService = {
-    getUsers: () => of([fakeUser]),
-    httpClient: {}
-  } as any;
+  // const fakeUserService = {
+  //   getUsers: () => of([fakeUser]),
+  //   httpClient: {}
+  // } as any;
+
+  // const fakeUserService = jasmine.createSpyObj('userService', ['getUsers']);
+  const userService = new UserService(null);
 
   beforeEach(() => {
-    component = new UsersComponent(fakeUserService);
+    component = new UsersComponent(userService);
   });
 
   it(`should have a component`, () => {
@@ -18,10 +22,15 @@ describe(`Users Component`, () => {
   });
 
   it(`should have a list of users`, () => {
+    // const spy = fakeUserService.getUsers.and.returnValue(of([fakeUser]));
+    const spy = spyOn(userService, 'getUsers').and.returnValue(of([fakeUser]));
     component.ngOnInit();
     component.users$.subscribe(users => {
       console.log(users);
       expect(users).toEqual([fakeUser]);
+      expect(spy).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalledWith();
+      expect(spy).toHaveBeenCalledTimes(1);
     });
   });
 });
