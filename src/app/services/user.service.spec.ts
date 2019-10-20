a7d2d876f596393110e97995926137eaa36a4bf5
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-10-20 10:34:19 +0200

    removed all tests

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,19 +1,1 @@
-import { TestBed, inject } from '@angular/core/testing';
 
-import { UserService } from './user.service';
-import { HttpClient } from '../../../node_modules/@angular/common/http';
-
-describe('UserService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-          UserService,
-          {provide: HttpClient , userValue: {}}
-        ]
-    });
-  });
-
-  it('should be created', inject([UserService], (service: UserService) => {
-    expect(service).toBeTruthy();
-  }));
-});
